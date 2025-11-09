c82e9d1d36f5cddd59c8c0279610312d938681ed
Caiofernando52 <caio.fernandogc@gmail.com>
2025-11-09 16:58:44 -0300

    permitir abrir detalhes ao clicar no carro

--- client/src/components/CarCard.tsx
+++ client/src/components/CarCard.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './../styles/car-card.scss';
 
-export default function CarCard({ image, brand, model }: any) {
+interface CarCardProps {
+  _id: string;
+  image: string;
+  brand: string;
+  model: string;
+}
+
+export default function CarCard({ image, brand, model, _id }: CarCardProps) {
   return (
-    <div className="car-card">
+    <Link to={`/cars/${_id}`} className="car-card">
       <img src={image} alt={model} />
       <h3>{brand}</h3>
       <p>{model}</p>
-    </div>
+    </Link>
   );
 }
